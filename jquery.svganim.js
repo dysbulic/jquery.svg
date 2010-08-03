0df9ffb3dc3fcf483bffec398c99e24dc55288bb
Will Holcomb <will@dhappy.org>
2010-08-03 14:38:14 -0400

    checkpoint

--- jquery.svganim.js
+++ jquery.svganim.js
@@ -7,30 +7,35 @@
 
 (function($) { // Hide scope, no $ conflict
 
+// Opacity support is tested on an anchor in a div and fails in svgs
+if( document.documentElement.nodeName == 'svg' ) {
+    $.support.opacity = true;
+}
+
 // Enable animation for all of these SVG numeric attributes -
 // named as svg-* or svg* (with first character upper case)
-$.each(['x', 'y', 'width', 'height', 'rx', 'ry', 'cx', 'cy', 'r', 'x1', 'y1', 'x2', 'y2',
-		'stroke-width', 'strokeWidth', 'opacity', 'fill-opacity', 'fillOpacity',
-		'stroke-opacity', 'strokeOpacity', 'font-size', 'fontSize'],
-	function(i, attrName) {
-		var ccName = attrName.charAt(0).toUpperCase() + attrName.substr(1);
-		$.fx.step['svg' + ccName] = $.fx.step['svg-' + attrName] = function(fx) {
-			var realAttrName = $.svg._attrNames[attrName] || attrName;
-			var attr = fx.elem.attributes.getNamedItem(realAttrName);
-			if (!fx.set) {
-				fx.start = (attr ? parseFloat(attr.nodeValue) : 0);
-				var offset = fx.options.curAnim['svg-' + attrName] ||
-					fx.options.curAnim['svg' + ccName];
-				if (/^[+-]=/.exec(offset)) {
-					fx.end = fx.start + parseFloat(offset.replace(/=/, ''));
-				}
-				$(fx.elem).css(realAttrName, '');
-				fx.set = true;
-			}
-			var value = (fx.pos * (fx.end - fx.start) + fx.start) + (fx.unit == '%' ? '%' : '');
-			(attr ? attr.nodeValue = value : fx.elem.setAttribute(realAttrName, value));
-		};
-	}
+$.each([ 'x', 'y', 'width', 'height', 'rx', 'ry', 'cx', 'cy', 'r', 'x1', 'y1', 'x2', 'y2',
+	 'stroke-width', 'strokeWidth', 'opacity', 'fill-opacity', 'fillOpacity',
+	 'stroke-opacity', 'strokeOpacity', 'font-size', 'fontSize' ],
+       function(i, attrName) {
+	   var ccName = attrName.charAt(0).toUpperCase() + attrName.substr(1);
+	   $.fx.step['svg' + ccName] = $.fx.step['svg-' + attrName] = function(fx) {
+	       var realAttrName = $.svg._attrNames[attrName] || attrName;
+	       var attr = fx.elem.attributes.getNamedItem(realAttrName);
+	       if (!fx.set) {
+		   fx.start = (attr ? parseFloat(attr.nodeValue) : 0);
+		   var offset = ( fx.options.curAnim['svg-' + attrName] ||
+				  fx.options.curAnim['svg' + ccName] );
+		   if (offset[1] == '=') {
+		       fx.end = fx.start + parseFloat(offset[0] + offset.substring(2));
+		   }
+		   $(fx.elem).css(realAttrName, '');
+		   fx.set = true;
+	       }
+	       var value = (fx.pos * (fx.end - fx.start) + fx.start) + (fx.unit == '%' ? '%' : '');
+	       (attr ? attr.nodeValue = value : fx.elem.setAttribute(realAttrName, value));
+	   };
+       }
 );
 
 // Enable animation for the SVG viewBox attribute
@@ -76,111 +81,90 @@ function parseViewBox(value) {
 	return viewBox;
 }
 
-// Enable animation for the SVG transform attribute
-$.fx.step['svgTransform'] = $.fx.step['svg-transform'] = function(fx) {
-	var attr = fx.elem.attributes.getNamedItem('transform');
-	if (!fx.set) {
-		fx.start = parseTransform(attr ? attr.nodeValue : '');
-		fx.end = parseTransform(fx.end, fx.start);
-		fx.set = true;
-	}
-	var transform = '';
-	for (var i = 0; i < fx.end.order.length; i++) {
-		switch (fx.end.order.charAt(i)) {
-			case 't':
-				transform += (fx.start.translateX != fx.end.translateX || fx.start.translateY != fx.end.translateY ?
-					' translate(' + (fx.pos * (fx.end.translateX - fx.start.translateX) + fx.start.translateX) + ',' +
-					(fx.pos * (fx.end.translateY - fx.start.translateY) + fx.start.translateY) + ')' : '');
-				break;
-			case 's':
-				transform += (fx.start.scaleX != fx.end.scaleX || fx.start.scaleY != fx.end.scaleY ? 
-					' scale(' + (fx.pos * (fx.end.scaleX - fx.start.scaleX) + fx.start.scaleX) + ',' +
-					(fx.pos * (fx.end.scaleY - fx.start.scaleY) + fx.start.scaleY) + ')' : '');
-				break;
-			case 'r':
-				transform += (fx.start.rotateA != fx.end.rotateA ||
-					fx.start.rotateX != fx.end.rotateX || fx.start.rotateY != fx.end.rotateY ?
-					' rotate(' + (fx.pos * (fx.end.rotateA - fx.start.rotateA) + fx.start.rotateA) + ',' +
-					(fx.pos * (fx.end.rotateX - fx.start.rotateX) + fx.start.rotateX) + ',' +
-					(fx.pos * (fx.end.rotateY - fx.start.rotateY) + fx.start.rotateY) + ')' : '');
-				break;
-			case 'x':
-				transform += (fx.start.skewX != fx.end.skewX ?
-					' skewX(' + (fx.pos * (fx.end.skewX - fx.start.skewX) + fx.start.skewX) + ')' : '');
-			case 'y':
-				transform += (fx.start.skewY != fx.end.skewY ?
-					' skewY(' + (fx.pos * (fx.end.skewY - fx.start.skewY) + fx.start.skewY) + ')' : '');
-				break;
-			case 'm':
-				var matrix = '';
-				for (var j = 0; j < 6; j++) {
-					matrix += ',' + (fx.pos * (fx.end.matrix[j] - fx.start.matrix[j]) + fx.start.matrix[j]);
-				}				
-				transform += ' matrix(' + matrix.substr(1) + ')';
-				break;
-		}
-	}
-	(attr ? attr.nodeValue = transform : fx.elem.setAttribute('transform', transform));
-};
+    $.fx.step['transform'] = function(fx) {
+        var attr = fx.elem.attributes.getNamedItem('transform');
+        if (!fx.set) {
+	    fx.start = parseTransform(attr ? attr.nodeValue : '');
+	    fx.end = parseTransform(fx.end, fx.start, fx.options.clear === false);
+	    fx.set = true;
+        }
+        var transforms = [];
+        $.each(fx.end.order, function(idx, op) {
+            transforms.push(op + '(' +
+                            $.map(fx.end[op], function(val, idx) {
+                                var start = (fx.start[op] && fx.start[op][idx] ?
+                                             fx.start[op][idx] :
+                                             (op == 'scale' ? 1 : 0));
+                                return start + fx.pos * (val - start);
+                            }).join(',') +
+                            ')');
+        });
+        var transform = transforms.join(' ');
+        (attr ? attr.nodeValue = transform : fx.elem.setAttribute('transform', transform));
+    }
 
-/* Decode a transform string and extract component values.
-   @param  value     (string) the transform string to parse
-   @param  original  (object) the settings from the original node
-   @return  (object) the combined transformation attributes */
-function parseTransform(value, original) {
-	value = value || '';
-	if (typeof value == 'object') {
-		value = value.nodeValue;
+/**
+ * Parses the transform attribute of a svg.
+ *
+ * This implementation preserves transformation order for application,
+ * but flattens the arguments, so transform(10) rotate(20) transform(-15)
+ * would come out as transform(-5) rotate(20) transform(-5).
+ *
+ * Avoiding this requires complex comparison to the original transform
+ * or the conversion of all transforms to matrices.
+ */
+function parseTransform(value, original, preserve) {
+    value = value || '';
+    if ( typeof value.nodeValue != 'undefined' ) {
+	value = value.nodeValue;
+    }
+
+    var transform = {};
+    transform.order = [];
+
+    var opPattern = /([a-zA-Z]+)\(([^\)]*)\)/g;
+    var numPattern = /((?:[+-]=?)?(?:\d+\.?\d*|\d*\.?\d+))/g;
+
+    while ( opResult = opPattern.exec(value) ) {
+	var op = opResult[1];
+	transform.order.push(op);
+	if ( !transform[op] ) {
+	    transform[op] = [];
 	}
-	var transform = $.extend({translateX: 0, translateY: 0, scaleX: 0, scaleY: 0,
-		rotateA: 0, rotateX: 0, rotateY: 0, skewX: 0, skewY: 0,
-		matrix: [0, 0, 0, 0, 0, 0]}, original || {});
-	transform.order = '';
-	var pattern = /([a-zA-Z]+)\(\s*([+-]?[\d\.]+)\s*(?:[\s,]\s*([+-]?[\d\.]+)\s*(?:[\s,]\s*([+-]?[\d\.]+)\s*(?:[\s,]\s*([+-]?[\d\.]+)\s*[\s,]\s*([+-]?[\d\.]+)\s*[\s,]\s*([+-]?[\d\.]+)\s*)?)?)?\)/g;
-	var result = pattern.exec(value);
-	while (result) {
-		switch (result[1]) {
-			case 'translate':
-				transform.order += 't';
-				transform.translateX = parseFloat(result[2]);
-				transform.translateY = (result[3] ? parseFloat(result[3]) : 0);
-				break;
-			case 'scale':
-				transform.order += 's';
-				transform.scaleX = parseFloat(result[2]);
-				transform.scaleY = (result[3] ? parseFloat(result[3]) : transform.scaleX);
-				break;
-			case 'rotate':
-				transform.order += 'r';
-				transform.rotateA = parseFloat(result[2]);
-				transform.rotateX = (result[3] ? parseFloat(result[3]) : 0);
-				transform.rotateY = (result[4] ? parseFloat(result[4]) : 0);
-				break;
-			case 'skewX':
-				transform.order += 'x';
-				transform.skewX = parseFloat(result[2]);
-				break;
-			case 'skewY':
-				transform.order += 'y';
-				transform.skewY = parseFloat(result[2]);
-				break;
-			case 'matrix':
-				transform.order += 'm';
-				transform.matrix = [parseFloat(result[2]), parseFloat(result[3]),
-					parseFloat(result[4]), parseFloat(result[5]),
-					parseFloat(result[6]), parseFloat(result[7])];
-				break;
-		}
-		result = pattern.exec(value);
-	}
-	return transform;
+	for ( count = 0; numResult = numPattern.exec(opResult[2]); count++ ) {
+            if ( typeof transform[op][count] == 'undefined' ) {
+                transform[op][count] = 0;
+            }
+            // Handle relative transforms ([+-]=)
+            if ( numResult[1][1] == '=' ) {
+                if ( original && original[op] && typeof original[op][count] != 'undefined' ) {
+                    transform[op][count] += original[op][count];
+                }
+                numResult[1] = numResult[1][0] + numResult[1].substring(2);
+            }
+            transform[op][count] += parseFloat(numResult[1]);
+        }
+        if ( original && original[op] ) {
+            for ( ; count < original[op].length; count++ ) {
+                // Skew is the only transform that is symmetric by default
+                transform[op][count] = (op == 'skew') ? transform[op][0] : 0;
+            }
+        }
+    }
+    if ( preserve && original ) {
+        // If the transform was not overriden, add it to the front of the list
+        var ops = $.map(original.order, function(op) { return transform.order.indexOf(op) < 0 ? op : null });
+        $.each(ops, function(idx, op) { transform[op] = original[op]; });
+        transform.order = $.merge(ops, transform.order);
+    }
+    return transform;
 }
 
 // Enable animation for all of these SVG colour properties - based on jquery.color.js
 $.each(['fill', 'stroke'],
 	function(i, attrName) {
 		var ccName = attrName.charAt(0).toUpperCase() + attrName.substr(1);
-		$.fx.step['svg' + ccName] = $.fx.step['svg-' + attrName] = function(fx) {
+		$.fx.step[attrName] = $.fx.step['svg' + ccName] = $.fx.step['svg-' + attrName] = function(fx) {
 			if (!fx.set) {
 				fx.start = getColour(fx.elem, attrName);
 				var toNone = (fx.end == 'none');
